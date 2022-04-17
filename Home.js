8045e3c0b906ccb72a9f28f900cbcc38860183fb
Theerapat6 <98305177+Theerapat6@users.noreply.github.com>
2022-04-17 17:18:18 +0700

    Add files via upload

--- Home.js
+++ Home.js
@@ -32,3 +32,27 @@ window.onscroll =() =>{
 }
 
 
+var swiper = new Swiper(".products-sli", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+       
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1020: {
+        slidesPerView: 3,
+      
+      },
+    },
+  });
+
