ba8b7de5b9a8ec82e6d9318dea518a339aa2fd23
Theerapat6 <98305177+Theerapat6@users.noreply.github.com>
2022-05-13 18:12:12 +0700

    Add files via upload

--- Home.js
+++ Home.js
@@ -3,7 +3,7 @@ let searchF = document.querySelector('.search-F');
 document.querySelector('#search').onclick =() =>{
     searchF.classList.toggle('active');
     loginF.classList.remove('active');
-    navbar.classList.remove('active');
+    
 }
 
 let loginF = document.querySelector('.login-F');
@@ -12,22 +12,13 @@ document.querySelector('#login').onclick =() =>{
     loginF.classList.toggle('active');
     searchF.classList.remove('active');
     
-    navbar.classList.remove('active');
 }
 
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchF.classList.remove('active');
-    loginF.classList.remove('active');
-    
-}
 
 window.onscroll =() =>{
     searchF.classList.remove('active');
     loginF.classList.remove('active');
-    navbar.classList.remove('active');
+    
 }
 
 
